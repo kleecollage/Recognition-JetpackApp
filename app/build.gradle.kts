2a58b3d57118d7fa4961b96c69c2d661d9d6f92f
Klee Collage <klee.collage@gmail.com>
2024-08-09 16:21:17 -0500

    Required dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    // coil para optimizar imagenes
+    implementation(libs.coil.compose)
+    // mlkit
+    implementation(libs.play.services.mlkit.text.recognition)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
