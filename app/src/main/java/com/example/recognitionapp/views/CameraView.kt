2188b25a4dc0b78192eb4874ffa35b92c76da2e2
Klee Collage <klee.collage@gmail.com>
2024-08-09 19:06:47 -0500

    Access to gallery photo and camera | Image text recognition implemented

--- app/src/main/java/com/example/recognitionapp/views/CameraView.kt
+++ app/src/main/java/com/example/recognitionapp/views/CameraView.kt
@@ -1,10 +1,122 @@
 package com.example.recognitionapp.views
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import coil.compose.rememberAsyncImagePainter
+import com.example.recognitionapp.R
 import com.example.recognitionapp.viewModel.ScannerViewModel
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Objects
 
 @Composable
 fun CameraView(viewModel: ScannerViewModel) {
-    Text(text = "Camera View is working")
+    val context = LocalContext.current
+    val clipBoard = LocalClipboardManager.current
+    val file = context.createImageFile()
+    var image by remember { mutableStateOf<Uri>(Uri.EMPTY) }
+    var imageDefault = R.drawable.ic_photo
+    val permissionCheckResult = ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA)
+
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        context.packageName + ".provider",
+        file
+    )
+
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture()) { image = uri }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) {
+        if (it != null) { // it = Uri = image path
+            viewModel.showToast(context, "Permiso concedido")
+            cameraLauncher.launch(uri)
+        } else {
+            viewModel.showToast(context, "Permiso denegado")
+        }
+    }
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(16.dp, 8.dp)
+                .clickable {
+                    if( permissionCheckResult == PackageManager.PERMISSION_GRANTED ) {
+                        cameraLauncher.launch(uri)
+                    } else {
+                        permissionLauncher.launch(android.Manifest.permission.CAMERA)
+                    }
+                },
+            painter = rememberAsyncImagePainter(
+                if (image.path?.isNotEmpty() == true) image else imageDefault
+            ),
+            contentDescription = null
+        ).apply {
+            viewModel.onRecognizedText(image, context)
+        }
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        val scrollState = rememberScrollState()
+        Text(
+            text = viewModel.recognizedText,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(scrollState)
+                .clickable {
+                    clipBoard.setText(AnnotatedString(viewModel.recognizedText))
+                    // viewModel.showToast(context, "Copiado")
+                }
+        )
+    }
+}
+
+@SuppressLint("SimpleDateFormat")
+fun Context.createImageFile(): File {
+    val timeStamp = SimpleDateFormat("yyyMMdd_HHmmss").format(Date())
+    val imageFileName = "JPEG_" + timeStamp + "_"
+
+    return File.createTempFile(
+        imageFileName,
+        ".jpg",
+        externalCacheDir
+    )
 }
--- app/src/main/java/com/example/recognitionapp/views/GaleryView.kt
+++ app/src/main/java/com/example/recognitionapp/views/GaleryView.kt
@@ -1,10 +1,84 @@
 package com.example.recognitionapp.views
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.recognitionapp.R
 import com.example.recognitionapp.viewModel.ScannerViewModel
 
 @Composable
 fun GaleryView(viewModel: ScannerViewModel) {
-    Text(text = "Galery View is working")
+    val context = LocalContext.current
+    val clipBoard = LocalClipboardManager.current
+
+    var image: Any? by remember { mutableStateOf(R.drawable.gallery) }
+
+    val photoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) {
+        if (it != null) { // it = Uri = image path
+            image = it
+            viewModel.onRecognizedText(image, context)
+        } else {
+            viewModel.showToast(context, "No se ha seleccionado ninguna imagen")
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(16.dp, 8.dp)
+                .clickable {
+                    photoPicker.launch(
+                        PickVisualMediaRequest(
+                            ActivityResultContracts.PickVisualMedia.ImageOnly
+                        )
+                    )
+                },
+            painter = rememberAsyncImagePainter(image),
+            contentDescription = null
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        val scrollState = rememberScrollState()
+        Text(
+            text = viewModel.recognizedText,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(scrollState)
+                .clickable {
+                    clipBoard.setText(AnnotatedString(viewModel.recognizedText))
+                    // viewModel.showToast(context, "Copiado")
+                }
+            )
+    }
 }
